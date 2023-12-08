f2eafa0bd9bf746df658baeb01d1567a44aecd80
Mjulianfr001056 <222112217@stis.ac.id>
2023-12-09 02:25:05 +0700

    Refactor PieChartComponent.vue to improve data rendering

--- src/components/PieChartComponent.vue
+++ src/components/PieChartComponent.vue
@@ -1,16 +1,10 @@
 <template>
-  <div>
-    <h2>Pie Chart Output</h2>
     <div>
-      <label for="selectedColumn">Pilih Kolom Data:</label>
-      <select v-model="selectedColumn" @change="updateChartData">
-        <option v-for="column in availableColumns" :key="column">{{ column }}</option>
-      </select>
+        <h2>Pie Chart Output</h2>
+        <div style="max-height: 700px" v-for="(data, index) in chartData" :key="index">
+            <Pie v-if="data" :data="data" :options="chartOptions" />
+        </div>
     </div>
-    <div style="max-height: 700px">
-      <Pie v-if="chartData" :data="chartData" :options="chartOptions" />
-    </div>
-  </div>
 </template>
 
 <script>
@@ -20,69 +14,53 @@ import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale, Li
 ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale, LinearScale);
 
 export default {
-  name: "PieChart",
-  components: { Pie },
-  props: {
-    passedData: {
-      type: Object,
-      required: true
-    }
-  },
-  data() {
-    return {
-      chartData: null,
-      chartOptions: {
-        responsive: true,
-        maintainAspectRatio: true,
-      },
-      Colors: ["#FFB3BA", "#FFDFBA", "#FFFFBA", "#BAFFC9", "#BAE1FF"],
-      availableColumns: [],
-      selectedColumn: null,
-      result: {}, // Define result object here
-    };
-  },
-  mounted() {
-    this.initializeChartData();
-  },
-  methods: {
-    initializeChartData() {
-      this.passedData.contents.forEach((item) => {
-        Object.keys(item).forEach((key) => {
-          if (!this.result[key]) {
-            this.result[key] = {
-              label: key,
-              total: 0,
-            };
-          }
-          this.result[key].total += item[key];
+    name: "PieChart",
+    components: { Pie },
+    props: {
+        passedData: {
+            type: Object,
+            required: true
+        }
+    },
+    data() {
+        return {
+            chartData: null,
+            chartOptions: {
+                responsive: true,
+                maintainAspectRatio: true,
+            },
+            colors: ["#FFB3BA", "#FFDFBA", "#FFFFBA", "#BAFFC9", "#BAE1FF"],
+        };
+    },
+    mounted() {
+        let result = {};
+
+        this.passedData.contents.forEach((item) => {
+            Object.keys(item).forEach((key) => {
+                if (!result[key]) {
+                    result[key] = {
+                        data: [],
+                    };
+                }
+                result[key].data.push(item[key]);
+            });
         });
-      });
 
-      this.availableColumns = Object.keys(this.result);
-      this.selectedColumn = this.availableColumns[0]; // Set default selected column
+        this.chartData = [];
 
-      this.updateChartData();
-    },
-    updateChartData() {
-      if (!this.selectedColumn) {
-        return;
-      }
-
-      const chartDataArray = Object.keys(this.result).map((key) => ({
-        label: this.result[key].label,
-        total: this.result[key].total,
-      }));
-
-      this.chartData = {
-        labels: chartDataArray.map((data) => data.label),
-        datasets: [
-          {
-            data: chartDataArray.map((data) => data.total),
-            backgroundColor: this.Colors.slice(0, chartDataArray.length),
-          },
-        ],
-      };
-    },
-  },
+        Object.keys(result).forEach( key => {
+            const label = this.passedData.xLabel;
+            
+            this.chartData.push({
+                labels: label,
+                datasets: [
+                    {
+                        backgroundColor: this.colors.slice(0, label.length),
+                        data: result[key].data,
+                    }
+                ],
+            });
+        });
+    }
 };
 </script>
